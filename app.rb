a7b73a05fe8f7bd313519d62f516f1a4d1d93967
tke578 <tke578@gmail.com>
2016-07-26 22:44:14 -0700

    Fixing

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'rest-client'
+require 'better_errors'
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = __dir__
+end
+
 
 enable :sessions
 
@@ -10,11 +17,11 @@ CLIENT_SECRET = ENV['CLIENT_SECRET']
 
 
 get '/' do
-
-  '<a href="https://api.instagram.com/oauth/authorize/?client_id=<%= CLIENT_ID %>&redirect_uri=<%= CALLBACK_URL %>&response_type=code">Connect with Instagram</a>'
+	link = "https://api.instagram.com/oauth/authorize/?client_id=#{CLIENT_ID}&redirect_uri=#{CALLBACK_URL}&response_type=code"
+  "<a href=#{link}>Connect with Instagram</a>"
 end
 
-get "/oauth/connect" do
+get "/oauth/callback" do
 	raise
   
 end
