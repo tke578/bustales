7e452dfe8ca86328ead1a900ebf44c5be615f6e0
Andrew G <andrew@sfrent.net>
2016-07-27 17:55:39 -0700

    Still in the works

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'rest-client'
 require 'better_errors'
+require "net/http"
+require "uri"
+
 
 configure :development do
   use BetterErrors::Middleware
@@ -22,6 +25,21 @@ get '/' do
 end
 
 get "/oauth/callback" do
-	raise
+	if params[:code]
+		uri = URI.parse("https://api.instagram.com")
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		request = Net::HTTP::Post.new("/oauth/access_token")
+		request.set_form_data({"client_id" => CLIENT_ID, "client_secret" => CLIENT_SECRET, "grant_type" => "authorization_code", "redirect_uri" => CALLBACK_URL, "code" => params[:code]})
+		response = http.request(request)
+		JSON.parse(response)
+		raise
+
+		# RestClient.get 'https://api.instagram.com/v1/users/self/media/recent/', {:params => {'access_token' => '417315829.cebe384.959e84ed50b64f5d8d033776418f169f'}}
+	end
+
+
+	
   
 end
